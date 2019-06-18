e4603188652f295d0e4347748491371eb9e26888
amsoft2017 <32521319+amsoft2017@users.noreply.github.com>
2019-06-18 18:54:57 +0500

    Создали плагин Tuturial Создали модели Category and Item

---
+++ plugins/amsoft/tutorial/updates/create_categories_table.php
@@ -0,0 +1,22 @@
+<?php namespace Amsoft\Tutorial\Updates;
+
+use Schema;
+use October\Rain\Database\Schema\Blueprint;
+use October\Rain\Database\Updates\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    public function up()
+    {
+        Schema::create('amsoft_tutorial_categories', function(Blueprint $table) {
+            $table->engine = 'InnoDB';
+            $table->increments('id');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('amsoft_tutorial_categories');
+    }
+}
---
+++ storage/cms/cache/d0/cc/home.htm.php
@@ -0,0 +1,5 @@
+<?php 
+class Cms5d08e20b9d8f2892956929_f2712e9cc22adc7c2d3f40b341d584d8Class extends Cms\Classes\PageCode
+{
+
+}
